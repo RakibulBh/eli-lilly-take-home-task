125ba73038aa75f3e59e1f1d9a2cb983d116b286
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-11-24 15:01:51 +0000

    feat: deleting medicines

--- frontend/components/edit-medicine-dialog.tsx
+++ frontend/components/edit-medicine-dialog.tsx
@@ -15,15 +15,19 @@ import { useState } from "react";
 import axios from "axios";
 import { Label } from "@radix-ui/react-label";
 import { toast } from "sonner";
-import { revalidatePath } from "next/cache";
+import { useRouter } from "next/navigation";
 
 const EditMedicineDialog = ({ medicine }: { medicine: Medicine }) => {
+  const router = useRouter();
   const [name, setName] = useState(medicine.name);
   const [price, setPrice] = useState<string>(String(medicine.price));
+  const [open, setOpen] = useState<boolean>(false);
 
-  const onSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const handleSubmit = () => {
+    if (!name || !price) {
+      toast.error("Please fill in all fields");
+      return;
+    }
     const formData = new URLSearchParams();
     formData.append("name", name);
     formData.append("price", price);
@@ -34,16 +38,36 @@ const EditMedicineDialog = ({ medicine }: { medicine: Medicine }) => {
           "Content-Type": "application/x-www-form-urlencoded",
         },
       });
-      revalidatePath("/");
+      toast.success("Medicine updated successfully");
+      router.refresh();
+      setOpen(false);
     } catch (error) {
-      toast("Error", {
-        description: "Error updating information",
+      toast.error("Failed to update medicine");
+    }
+  };
+
+  const handleDelete = async () => {
+    const formData = new URLSearchParams();
+    formData.append("name", name);
+
+    try {
+      await axios.delete("http://0.0.0.0:8000/delete", {
+        data: formData.toString(),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       });
+
+      toast.success("Successfully deleted!");
+      router.refresh();
+      setOpen(false);
+    } catch (error) {
+      toast.error("Error deleting medicine");
     }
   };
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>
         <MedicineComponent key={medicine.name} medicine={medicine} />
       </DialogTrigger>
@@ -51,7 +75,7 @@ const EditMedicineDialog = ({ medicine }: { medicine: Medicine }) => {
         <DialogHeader>
           <DialogTitle>Edit Medicine Details</DialogTitle>
         </DialogHeader>
-        <form onSubmit={onSubmit} className="space-y-4">
+        <div className="space-y-4">
           <div className="space-y-1">
             <Label htmlFor="name">Name</Label>
             <Input
@@ -73,9 +97,12 @@ const EditMedicineDialog = ({ medicine }: { medicine: Medicine }) => {
             />
           </div>
           <div className="flex justify-end">
-            <Button>Submit</Button>
+            <div className="flex gap-2">
+              <Button onClick={handleDelete}>Delete</Button>
+              <Button onClick={handleSubmit}>Submit</Button>
+            </div>
           </div>
-        </form>
+        </div>
       </DialogContent>
     </Dialog>
   );
